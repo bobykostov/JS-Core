198bca8e2bcc29b42e2f6648088c43c7f13c4d30
Bozhidar Kostov <37141459+bobykostov@users.noreply.github.com>
2019-02-21 15:24:12 +0200

    Update 04. Length Limit.js

--- JavaScript Advanced/13. Classes and Members - EXERCISE/04. Length Limit.js
+++ JavaScript Advanced/13. Classes and Members - EXERCISE/04. Length Limit.js
@@ -31,13 +31,13 @@ class Stringer {
 
 
 let test = new Stringer('Test', 5);
-console.log(test.toString());
+console.log(test.toString()); //Test
 
 test.decrease(3);
-console.log(test.toString());
+console.log(test.toString()); //Te..
 
 test.decrease(5);
-console.log(test.toString());
+console.log(test.toString()); //...
 
 test.increase(4);
-console.log(test.toString());
+console.log(test.toString()); //Test
