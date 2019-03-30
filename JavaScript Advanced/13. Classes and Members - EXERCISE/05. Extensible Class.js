7bd2e5a99c7aac939d3f75c13ca7204b8083d031
Bozhidar Kostov <37141459+bobykostov@users.noreply.github.com>
2019-03-30 18:09:41 +0200

    Create 05. Extensible Class.js

---
+++ JavaScript Advanced/13. Classes and Members - EXERCISE/05. Extensible Class.js
@@ -0,0 +1,25 @@
+let Extensible = (function () {
+    let id = 0;
+
+    return class Extensible {
+        constructor() {
+            this.id = id++;
+        }
+
+        extend(template) {
+            for (let key in template) {
+                if (typeof template[key] === 'function')
+                    Extensible.prototype[key] = template[key];
+                else this[key] = template[key];
+            }
+        }
+    };
+})();
+
+let obj1 = new Extensible();
+let obj2 = new Extensible();
+let obj3 = new Extensible();
+
+console.log(obj1.id);
+console.log(obj2.id);
+console.log(obj3.id);
